e5e6841636e87caab57472d9546ca9c779aebbc9
Chirag190820 <122150956+Chirag190820@users.noreply.github.com>
2023-09-20 14:01:35 +0530

    Add files via upload

---
+++ src/main/java/edu/project/jobportal/Job_Portal/exception/ApplicantNotfoundByIdException.java
@@ -0,0 +1,11 @@
+package edu.project.jobportal.Job_Portal.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@SuppressWarnings("serial")
+@Getter
+@AllArgsConstructor
+public class ApplicantNotfoundByIdException extends RuntimeException {
+	private String message;
+}
